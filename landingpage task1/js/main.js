1affb90ae4e735ba0f4b4969cdb256020f0d3619
Moyinoluwa E. Ayeni <142138464+therealemmy@users.noreply.github.com>
2023-12-19 00:09:26 +0100

    Add files via upload

---
+++ landingpage task1/js/main.js
@@ -0,0 +1,94 @@
+
+ScrollReveal().reveal('.header', {
+    delay: 400,
+    distance: '100%',
+    origin: 'left',
+    duration: 2000,
+    // easing: 'ease-in-out',
+});
+
+
+ScrollReveal().reveal('.left', {
+    delay: 400,
+    distance: '100%',
+    origin: 'left',
+    duration: 2000,
+    // easing: 'ease-in-out',
+});
+
+
+ScrollReveal().reveal('.right', {
+    delay: 400,
+    distance: '100%',
+    origin: 'right',
+    duration: 2000,
+    // easing: 'ease-in-out',
+});
+ScrollReveal().reveal('.header', {
+    delay: 400,
+    distance: '100%',
+    origin: 'left',
+    duration: 2000,
+    // easing: 'ease-in-out',
+});
+
+
+ScrollReveal().reveal('.left', {
+    delay: 400,
+    distance: '100%',
+    origin: 'left',
+    duration: 2000,
+    // easing: 'ease-in-out',
+});
+
+ScrollReveal().reveal('.right', {
+    delay: 400,
+    distance: '100%',
+    origin: 'right',
+    duration: 2000,
+    // easing: 'ease-in-out',
+});
+
+
+ScrollReveal().reveal('.btw', {
+    delay: 400,
+    distance: '100%',
+    origin: 'right',
+    duration: 2000,
+    // easing: 'ease-in-out',
+});
+
+
+ScrollReveal().reveal('.aboutimg', {
+    delay: 400,
+    distance: '100%',
+    origin: 'left',
+    duration: 2000,
+    // easing: 'ease-in-out',
+});
+
+
+ScrollReveal().reveal('.abouttext', {
+    delay: 400,
+    distance: '100%',
+    origin: 'right',
+    duration: 2000,
+    // easing: 'ease-in-out',
+});
+
+ScrollReveal().reveal('.homes', {
+    delay: 400,
+    distance: '100%',
+    origin: 'right',
+    duration: 2000,
+    // easing: 'ease-in-out',
+});
+
+ScrollReveal().reveal('.popular', {
+    delay: 400,
+    distance: '100%',
+    origin: 'left',
+    duration: 2000,
+    // easing: 'ease-in-out',
+});
+
