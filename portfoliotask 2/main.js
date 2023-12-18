bfa6472ced742a2ad2e3d280562a0e525ebc0050
Moyinoluwa E. Ayeni <142138464+therealemmy@users.noreply.github.com>
2023-12-19 00:10:01 +0100

    Add files via upload

---
+++ portfoliotask 2/main.js
@@ -0,0 +1,36 @@
+ScrollReveal().reveal('#image', {
+  delay: 10,
+  distance: '100%',
+  origin: 'left',
+  // easing: 'ease-in-out',
+});
+
+ScrollReveal().reveal('#aboutxt', {
+  delay: 10,
+  distance: '100%',
+  origin: 'right',
+  // easing: 'ease-in-out',
+});
+
+ScrollReveal().reveal('.picture', {
+  delay: 10,
+  distance: '100%',
+  origin: 'right',
+  // easing: 'ease-in-out',
+});
+
+
+ScrollReveal().reveal('.picture', {
+  delay: 10,
+  distance: '100%',
+  origin: 'right',
+  // easing: 'ease-in-out',
+});
+
+
+ScrollReveal().reveal('.text', {
+  delay: 10,
+  distance: '100%',
+  origin: 'left',
+  // easing: 'ease-in-out',
+});
